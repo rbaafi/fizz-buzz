6c6200d6883bad2ff4b6e23b313c89701dd852a7
Richmond <rbaafi@cnm.edu>
2020-01-16 10:32:43 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive;
+
+public class FizzBuzz {
+  }
+
+  public static String aResult() {
+    String result;
+
+    for(int i = 1; i <= 100; ++i) {
+
+      if (i % 15 == 0) {
+        System.out.println("FizzBuzz");
+      } else if (i % 3 ==0) {
+        System.out.println("Buzz");
+      } else if (i % 5 == 0) {
+        System.out.println();
+        }
+      }
+    }
+  }
+  
+}
